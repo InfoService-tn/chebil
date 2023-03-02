13a3895aa6a4f3f6b1c9e7d2e6c3d8699ac19c98
Infoservice-tn <info.service@gnet.tn>
2023-03-02 21:45:42 +0100

    Chebil 1.0

---
+++ l10n_tn_stamp/models/company.py
@@ -0,0 +1,28 @@
+from odoo import models, fields, api
+
+class CompanyStampTaxl(models.Model):
+    _inherit = 'res.company'
+
+    stamp_tax_enable = fields.Boolean (string="Timbre Fiscal")
+    stamp_tax_amount = fields.Monetary(string="Montant")
+    stamp_tax_sales_account = fields.Many2one('account.account', string="Compte Ventes")
+    stamp_tax_purchase_account = fields.Many2one('account.account', string="Compte Achats")
+    
+    def get_values(self):
+        res = super(CompanyStampTax, self).get_values()
+        res.update(
+            stamp_tax_enable = self.env['ir.config_parameter'].sudo().get_param('stamp_tax_enable'),
+            stamp_tax_amount = float(self.env['ir.config_parameter'].sudo().get_param('stamp_tax_amount')),
+            stamp_tax_sales_account = int(self.env['ir.config_parameter'].sudo().get_param('stamp_tax_sales_account')),
+            stamp_tax_purchase_account = int(self.env['ir.config_parameter'].sudo().get_param('stamp_tax_purchase_account')),
+                    )
+        return res
+
+    def set_values(self):
+        super(CompanyStampTax, self).set_values()
+        self.env['ir.config_parameter'].set_param('stamp_tax_enable', self.stamp_tax_enable)
+        if self.stamp_tax_enable:
+            self.env['ir.config_parameter'].set_param('stamp_tax_amount', self.stamp_tax_amount)
+            self.env['ir.config_parameter'].set_param('stamp_tax_sales_account', self.stamp_tax_sales_account.id)
+            self.env['ir.config_parameter'].set_param('stamp_tax_purchase_account', self.stamp_tax_purchase_account.id)
+            

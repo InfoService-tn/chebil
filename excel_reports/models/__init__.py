d4707a38184ebaf54829df514aba43c31666fb02
Infoservice-tn <info.service@gnet.tn>
2023-12-06 16:09:37 +0100

    Chebil 1.0

--- excel_reports/models/__init__.py
+++ excel_reports/models/__init__.py
@@ -20,5 +20,5 @@
 #
 #############################################################################
 from . import account_move
-# from . import sale_order
-# from . import stock_picking
+from . import sale_order
+from . import stock_picking

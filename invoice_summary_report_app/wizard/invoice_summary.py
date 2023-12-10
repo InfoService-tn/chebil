729c17edb82d815fe64ddc58fa9d1610e544cbee
Infoservice-tn <info.service@gnet.tn>
2023-12-10 22:01:27 +0100

    Chebil 1.0

--- invoice_summary_report_app/wizard/invoice_summary.py
+++ invoice_summary_report_app/wizard/invoice_summary.py
@@ -75,6 +75,7 @@ class InvoiceSummary(models.TransientModel):
 								'amount_total': records.amount_total_signed,
 								'amount_paid' : records.amount_paid,
 								'amount_residual' : records.amount_residual,
+								'invoice_lines': records.invoice_line_ids,
 							})
 					elif invoice_status == 'posted':
 
@@ -89,6 +90,8 @@ class InvoiceSummary(models.TransientModel):
 								'amount_total': records.amount_total_signed,
 								'amount_paid' : records.amount_paid,
 								'amount_residual' : records.amount_residual,
+								'invoice_lines': records.invoice_line_ids,
+
 							})
 			
 					partner_data.append(value)
@@ -110,6 +113,7 @@ class InvoiceSummary(models.TransientModel):
 								'amount_total': records.amount_total_signed,
 								'amount_paid' : credit_record.amount_paid,
 								'amount_residual' : credit_record.amount_residual,
+								'invoice_lines': records.invoice_line_ids,
 							})
 					elif invoice_status == 'posted':
 
@@ -124,6 +128,7 @@ class InvoiceSummary(models.TransientModel):
 								'amount_total': records.amount_total_signed,
 								'amount_paid' : credit_record.amount_paid,
 								'amount_residual' : credit_record.amount_residual,
+								'invoice_lines': records.invoice_line_ids,
 							})
 					partner_data.append(credit_value)
 
@@ -143,6 +148,7 @@ class InvoiceSummary(models.TransientModel):
 								'amount_total': records.amount_total_signed,
 								'amount_paid' : bill_record.amount_paid,
 								'amount_residual' : bill_record.amount_residual,
+								'invoice_lines': records.invoice_line_ids,
 							})
 							# print('bill_note------------------------' ,bill_note)
 					elif invoice_status== 'posted':
@@ -157,6 +163,7 @@ class InvoiceSummary(models.TransientModel):
 								'amount_total': records.amount_total_signed,
 								'amount_paid' : bill_record.amount_paid,
 								'amount_residual' : bill_record.amount_residual,
+								'invoice_lines': records.invoice_line_ids,
 							})
 					partner_data.append(bill_note)
 
@@ -176,6 +183,7 @@ class InvoiceSummary(models.TransientModel):
 								'amount_total': records.amount_total_signed,
 								'amount_paid' :note_record.amount_paid,
 								'amount_residual' : note_record.amount_residual,
+								'invoice_lines': records.invoice_line_ids,
 							})
 					elif invoice_status == 'posted':
 						if note_record.move_type=='in_refund' and note_record.state=='posted':
@@ -189,6 +197,7 @@ class InvoiceSummary(models.TransientModel):
 								'amount_total': records.amount_total_signed,
 								'amount_paid' : note_record.amount_paid,
 								'amount_residual' : note_record.amount_residual,
+								'invoice_lines': records.invoice_line_ids,
 							})
 					partner_data.append(refund_note)
 
@@ -238,14 +247,14 @@ class InvoiceSummary(models.TransientModel):
 		end_date = datetime.strptime(str(data.get('end_date', False)), '%Y-%m-%d').date()
 		to_date = end_date.strftime('%d-%m-%Y')
 
-		worksheet.merge_range((rowscol + 2), 0, (rowscol + 2), 1,'Start Date', title_format)
-		worksheet.merge_range((rowscol + 3), 0, (rowscol + 3), 1, str(from_date) , title_format)
+		# worksheet.merge_range((rowscol + 2), 0, (rowscol + 2), 1,'Start Date', title_format)
+		# worksheet.merge_range((rowscol + 3), 0, (rowscol + 3), 1, str(from_date) , title_format)
 
-		worksheet.merge_range((rowscol + 2), 4, (rowscol + 2), 5,'End Date', title_format)
-		worksheet.merge_range((rowscol + 3), 4, (rowscol + 3), 5, str(to_date) , title_format)
+		# worksheet.merge_range((rowscol + 2), 4, (rowscol + 2), 5,'End Date', title_format)
+		# worksheet.merge_range((rowscol + 3), 4, (rowscol + 3), 5, str(to_date) , title_format)
 		rowscol  = rowscol
 
-		row=4;
+		row=3
 		worksheet.write(row+2,0,"Invoice No",cell_format)
 		worksheet.write(row+2,1,"Date",cell_format)
 		worksheet.write(row+2,2,"Partner", cell_format)
@@ -255,25 +264,35 @@ class InvoiceSummary(models.TransientModel):
 		worksheet.write(row+2,6,"Total", cell_format)
 		# worksheet.write(row+2,4,"Amount Paid",cell_format)
 		# worksheet.write(row+2,5,"Amount Due",cell_format)
-		rows = (rowscol + 5)
+		rows = (rowscol + 2)
 		rowscol1 = rows +1
 		rows = rowscol1
 		partner = ''
 		# rowscol1 = rows + 2
 		for records in self._get_invoice_details(data, partner):
 			for record in records.get('partner_data'):
-				worksheet.write(rows, 0,  record.get('name'), cell_wrap_format)
-				worksheet.write(rows, 1,  str(record.get('invoice_date')), cell_wrap_format)
-				worksheet.write(rows, 2, record.get('partner_id'), cell_wrap_format)
+				if record.get ('name')!='':
+					worksheet.write(rows, 0, record.get('name'), cell_wrap_format)
+					worksheet.write(rows, 1, str(record.get('invoice_date')), cell_wrap_format)
+					worksheet.write(rows, 2, record.get('partner_id'), cell_wrap_format)
+					worksheet.write(rows, 3, record.get('amount_untaxed'), cell_wrap_format)
+					worksheet.write(rows, 4, record.get('amount_tax'), cell_wrap_format)
+					worksheet.write(rows, 5, record.get('amount_stamptax'), cell_wrap_format)
+					worksheet.write(rows, 6, record.get('amount_total'), cell_wrap_format)
+					# worksheet.write(rows, 4,  record.get('amount_paid'), cell_wrap_format)
+					# worksheet.write(rows, 5,  record.get('amount_residual'), cell_wrap_format)
+					rows = rows + 1
 
-				worksheet.write(rows, 3, record.get('amount_untaxed'), cell_wrap_format)
-				worksheet.write(rows, 4, record.get('amount_tax'), cell_wrap_format)
-				worksheet.write(rows, 5, record.get('amount_stamptax'), cell_wrap_format)
+					# invoice_lines = record.get ('invoice_lines')
+					for line in record.get ('invoice_lines'):
+						worksheet.write(rows, 0, line.name, cell_wrap_format)
+						worksheet.write(rows, 1, line.quantity, cell_wrap_format)
+						worksheet.write(rows, 2, line.price_unit, cell_wrap_format)
+						worksheet.write(rows, 3, line.discount cell_wrap_format)
+						worksheet.write(rows, 4, line.tax_ids, cell_wrap_format)
+						worksheet.write(rows, 5, line.amount, cell_wrap_format)
+						rows = rows + 1
 
-				worksheet.write(rows, 6,  record.get('amount_total'), cell_wrap_format)
-				# worksheet.write(rows, 4,  record.get('amount_paid'), cell_wrap_format)
-				# worksheet.write(rows, 5,  record.get('amount_residual'), cell_wrap_format)
-				rows = rows + 1
 			rows = rows
 		workbook.close()
 		partner_details = base64.b64encode(open('/tmp/' + file_path, 'rb+').read())
